ee66463262fc5d1d810a6e7bd3b008a0a567ec87
nermin <nermin@192.168.1.7>
2019-06-26 10:25:38 +0200

    add panel icon from theme font icon

--- classes/class-kemet-addons.php
+++ classes/class-kemet-addons.php
@@ -92,7 +92,6 @@ if ( ! class_exists('Kemet_Addons' ) ) {
         }
         
         public function kmt_admin_styles() {
-            wp_enqueue_style( 'kmt_admin_icon', KEMET_ADDONS_URL .'assets/admin/fonts/admin-icon-font/style.css');
             wp_enqueue_style( 'kmt_admin_css', KEMET_ADDONS_URL .'assets/admin/css/style.css' );
         }  
 
