6e602b52f64a4b352655d447dd5a2bbc36ee35e8
nermin <nermin@192.168.1.7>
2019-10-01 14:58:50 +0200

    minification enhancement

--- classes/class-kemet-minify.php
+++ classes/class-kemet-minify.php
@@ -7,20 +7,15 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
 if ( ! class_exists( 'Kemet_Minify' ) ) {
-
 	/**
 	 * Kemet_Minify
 	 */
 	class Kemet_Minify {
-
 		static private $js_files = array();
-
 		static private $css_files = array();
         
         private static $instance;
-
 		/**
 		 * Initiator
 		 *
@@ -32,43 +27,13 @@ if ( ! class_exists( 'Kemet_Minify' ) ) {
 			if ( ! isset( self::$instance ) ) {
 				self::$instance = new self();
 			}
-
 			return self::$instance;
 		}
-
 		public function __construct() {
             add_action( 'customize_save_after', __CLASS__ . '::refresh_assets', 11 );
 			add_action( 'wp_enqueue_scripts', array( $this, 'merge_all_scripts'));
-            //wp_add_inline_style( 'kemet-addon-css', apply_filters( 'kemet_addons_dynamic_css', '' ) );
-           // add_action( 'wp_enqueue_scripts', 'custom_titles_color' );
-//            function custom_titles_color() {
-////                $header_5_icon_color         = kemet_get_option( 'header-5-icon-color' );
-////                $css_output = array(     
-////                '.kemet-addons-header5 .animated-icon span,.kemet-addons-header7 .animated-icon span, body' => array(
-////					'background-color' => esc_attr($header_5_icon_color),
-////                ),
-////           );
-////                $parse_css = kemet_parse_css( $css_output );
-////            $dynamic_css = $parse_css;
-////            
-////           // return $dynamic_css . $adv_footer_css_output;
-////           // return $dynamic_css;
-////
-//////            $css = '
-//////                body {
-//////                    background-color: red;
-//////                }';
-//////
-////            wp_register_style( 'kemet-addon-css', false );
-////            wp_enqueue_style( 'kemet-addon-css' );
-////            //wp_add_inline_style( 'dma-inline-style', $dynamic_css );
-////                wp_add_inline_style( 'kemet-addon-css', apply_filters( 'kemet_addons_dynamic_css', '' ) );
-//
-//        }
 		}
         
-
-
 		public function merge_all_scripts() 
 		{
 			$css_files = self::get_css_files();
@@ -92,18 +57,12 @@ if ( ! class_exists( 'Kemet_Minify' ) ) {
 				$css_file_path = $handle;
 				$merged_style .=  file_get_contents($css_file_path)."\n";
 			}
-
 			file_put_contents ( $merged_js_file_location , $merged_script);
            // chmod($filename, 0664);
 			file_put_contents ( $merged_css_file_location , $merged_style);
            // chmod($merged_css_file_location, "0664");
-
 			wp_enqueue_script('merged-script', KEMET_ADDONS_URL . 'merged-script.js', array('jquery'), '1.0.0', true );
 			wp_enqueue_style('merged-style', KEMET_ADDONS_URL . 'merged-style.css');
-            //wp_add_inline_style( 'merged-style', apply_filters( 'kemet_dynamic_css', '' ) );
-			//wp_add_inline_style( 'kemet_addons_dynamic_css', apply_filters( 'kemet_addon_dynamic_css', 20 ) );
-           // wp_add_inline_style( 'astra-addon-css', apply_filters( 'astra_dynamic_css', '' ) );
-            //wp_add_inline_style( 'kemet-addon-css', apply_filters( 'kemet_dynamic_css', '' ) );
             wp_register_style( 'kemet-addon-css', false );
             wp_enqueue_style( 'kemet-addon-css' );
             //wp_add_inline_style( 'dma-inline-style', $dynamic_css );
@@ -111,22 +70,14 @@ if ( ! class_exists( 'Kemet_Minify' ) ) {
             
             
 		}
-        public function enqueue_scripts() {
-           // wp_add_inline_style( 'kemet-addons-css', apply_filters( 'kemet_dynamic_css' ) , '');
-         //   wp_add_inline_style( 'kemet-addon-css', apply_filters( 'kemet_addons_dynamic_css', '' ) );
-        }
 
 		static public function get_css_files() {
-
 			if ( 1 > count( self::$css_files ) ) {
 				do_action( 'kemet_get_css_files' );		
 			}
-
 			return apply_filters( 'kemet_kmt_add_css_file', self::$css_files );
 		}
-
 		static public function get_js_files() {
-
 			if ( 1 > count( self::$js_files ) ) {
 				do_action( 'kemet_get_js_files' );
 				
@@ -135,23 +86,19 @@ if ( ! class_exists( 'Kemet_Minify' ) ) {
 		}
         
         public static function get_dynamic_css() {
-
 			// Get the cache dir and css key.
 			$cache_dir = self::get_cache_dir();
 			$css_slug  = self::_asset_slug();
-
 			// No css data, recompile the css.
 			if ( ! $css_data ) {
 				self::render_css();
 				return self::get_dynamic_css();
 			}
-
 			// Return the url.
 			return $css_data;
 		}
         
 	
-
 		/**
 		 * Used to add enqueue frontend styles.
 		 *
@@ -177,7 +124,6 @@ if ( ! class_exists( 'Kemet_Minify' ) ) {
 		 * @return void
 		 */
 		static public function kmt_add_js( $src = null, $handle = false ) {
-
 			if ( false != $handle ) {
 				self::$js_files[ $handle ] = $src;
 			} else {
@@ -190,6 +136,5 @@ if ( ! class_exists( 'Kemet_Minify' ) ) {
 	}
 	//new Kemet_Minify;
     Kemet_Minify::get_instance();
-
 }
 	
