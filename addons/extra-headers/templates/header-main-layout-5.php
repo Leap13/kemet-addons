73d4f0aac91289da9e6a62068d2eb288d5e6a8b3
Nermin Phelopis <nermin@Nermins-iMac.local>
2019-12-16 08:43:48 +0200

    sticky-header with header 5 issue

--- addons/extra-headers/templates/header-main-layout-5.php
+++ addons/extra-headers/templates/header-main-layout-5.php
@@ -1,3 +1,4 @@
+  
 <?php
 /* 
  * Header 5 Layout
@@ -7,12 +8,10 @@
 	<div class="main-header-bar">
         <?php kemet_main_header_bar_top(); ?>
         <div class="header-logo-menu-icon">
-                <div class="kmt-navbar-collapse">
-                    <?php kemet_primary_navigation_markup(); ?>
-                </div>
+            <?php if(wp_is_mobile()){ ?>
                 <div class="main-header-container logo-menu-icon">
-                    <div class="kmt-container">
-                    <?php kemet_site_branding_markup(); ?>
+                    <div class="kmt-container">   
+                    <?php kemet_site_branding_markup(); ?> 
                         <div class="menu-icon-social">
                             <div class="menu-icon">
                                 <a id="nav-icon" class="icon-bars-btn">
@@ -26,7 +25,34 @@
                             </div>
                         </div>
                     </div>
+                    <?php kemet_toggle_buttons_markup(); ?>
                 </div>
+                <div class="kmt-navbar-collapse">
+                    <?php kemet_primary_navigation_markup(); ?>
+                </div>
+                <?php }else{ ?>
+                    <div class="kmt-navbar-collapse">
+                    <?php kemet_primary_navigation_markup(); ?>
+                    </div>
+                    <div class="main-header-container logo-menu-icon">
+                    <div class="kmt-container">   
+                    <?php kemet_site_branding_markup(); ?> 
+                        <div class="menu-icon-social">
+                            <div class="menu-icon">
+                                <a id="nav-icon" class="icon-bars-btn">
+                                  <span></span>
+                                  <span></span>
+                                  <span></span>
+                                </a>
+                            </div>
+                            <div class="social-icons">
+                                <?php echo kemet_header_custom_item_outside_menu(); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <?php kemet_toggle_buttons_markup(); ?>
+                </div>
+                <?php } ?>
         </div><!-- Header Layout 5 -->
         <?php kemet_main_header_bar_bottom(); ?>
     </div> 
