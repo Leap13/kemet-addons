a8b339b11e100d55f792b1f56d40fd469b05bdf0
AbdelRahman <abdelrahman@Remon-Mac-mini.local>
2020-01-13 17:33:12 +0200

    header5-improved

--- addons/extra-headers/templates/header-main-layout-5.php
+++ addons/extra-headers/templates/header-main-layout-5.php
@@ -9,57 +9,32 @@ $icon_label = trim( apply_filters( 'icon_header_label', kemet_get_option( 'heade
 	<div class="main-header-bar">
         <?php kemet_main_header_bar_top(); ?>
         <div class="header-logo-menu-icon">
-            <?php if(wp_is_mobile()){ ?>
-                <div class="main-header-container logo-menu-icon">
-                    <div class="kmt-container">   
-                    <?php kemet_site_branding_markup(); ?> 
-                        <div class="menu-icon-social">
-                            <div class="menu-icon">
-                                <a id="nav-icon" class="icon-bars-btn">
-                                  <span></span>
-                                  <span></span>
-                                  <span></span>
-                                </a>
-                                <?php if(!empty($icon_label)){ ?>
-                                    <span class="header-icon-label"><?php echo esc_html( $icon_label ); ?></span>
-                                <?php } ?> 
-                            </div>
-                            <div class="social-icons">
-                                <?php echo kemet_header_custom_item_outside_menu(); ?>
-                            </div>
-                        </div>
-                    </div>
-                    <?php kemet_toggle_buttons_markup(); ?>
-                </div>
                 <div class="kmt-navbar-collapse">
-                    <?php kemet_primary_navigation_markup(); ?>
+                <?php kemet_primary_navigation_markup(); ?>
                 </div>
-                <?php }else{ ?>
-                    <div class="kmt-navbar-collapse">
-                    <?php kemet_primary_navigation_markup(); ?>
-                    </div>
-                    <div class="main-header-container logo-menu-icon">
-                    <div class="kmt-container">   
-                    <?php kemet_site_branding_markup(); ?> 
-                        <div class="menu-icon-social">
-                            <div class="menu-icon">
-                                <a id="nav-icon" class="icon-bars-btn">
-                                  <span></span>
-                                  <span></span>
-                                  <span></span>
-                                </a>
-                                <?php if(!empty($icon_label)){ ?>
-                                    <span class="header-icon-label"><?php echo esc_html( $icon_label ); ?></span>
-                                <?php } ?> 
-                            </div>
-                            <div class="social-icons">
-                                <?php echo kemet_header_custom_item_outside_menu(); ?>
-                            </div>
+                <div class="main-header-container logo-menu-icon">
+                <div class="kmt-container">   
+                 <div class="mobile-icon-logo">   
+                <?php kemet_site_branding_markup(); ?> 
+                <?php kemet_toggle_buttons_markup(); ?>
+                 </div>
+                    <div class="menu-icon-social">
+                        <div class="menu-icon">
+                            <a id="nav-icon" class="icon-bars-btn">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </a>
+                            <?php if(!empty($icon_label)){ ?>
+                                <span class="header-icon-label"><?php echo esc_html( $icon_label ); ?></span>
+                            <?php } ?> 
+                        </div>
+                        <div class="social-icons">
+                            <?php echo kemet_header_custom_item_outside_menu(); ?>
                         </div>
                     </div>
-                    <?php kemet_toggle_buttons_markup(); ?>
                 </div>
-                <?php } ?>
+            </div>
         </div><!-- Header Layout 5 -->
         <?php kemet_main_header_bar_bottom(); ?>
     </div> 
