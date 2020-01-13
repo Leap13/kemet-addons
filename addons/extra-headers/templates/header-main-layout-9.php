849806ca6210cfb2a2a93a391a91814d7e65dc76
AbdelRahman <abdelrahman@Remon-Mac-mini.local>
2020-01-13 12:53:43 +0200

    outside-menu-header9

--- addons/extra-headers/templates/header-main-layout-9.php
+++ addons/extra-headers/templates/header-main-layout-9.php
@@ -2,7 +2,6 @@
 /* 
  * Header 9 Layout
  */
-
 $icon_label = trim( apply_filters( 'icon_header_label', kemet_get_option( 'header-icon-label' ) ) );
 ?>
 
@@ -12,9 +11,15 @@ $icon_label = trim( apply_filters( 'icon_header_label', kemet_get_option( 'heade
     
         <div class="kmt-container">
             <div id="header-layout-9" class="header inline-icon-menu-header">
-                <div class="header9-logo-menu">
+                <div class="menu-icon">
                     <div class="inline-logo-menu"> 
                         <?php kemet_site_branding_markup(); ?>
+                            <?php if(wp_is_mobile()){
+                            echo '<div class="mobile-icon-outside-menu">' ;   
+                            kemet_toggle_buttons_markup();
+                            echo kemet_header_custom_item_outside_menu();
+                            echo '</div>' ;
+                            } ?>
                             <div class="menu-icon-social">
                                 <div class="menu-icon">
                                     <a id="nav-icon" class="icon-bars-btn">
@@ -27,15 +32,16 @@ $icon_label = trim( apply_filters( 'icon_header_label', kemet_get_option( 'heade
                                 <?php } ?>    
                                 </div>
                             </div>
-                        </div>
+                        
                         <div class="kmt-flex main-header-container">
                             <?php kemet_primary_navigation_markup(); ?>
                         </div>
                     </div>
-                    <div>
-                    <?php kemet_toggle_buttons_markup(); ?>   
-                    <?php echo kemet_header_custom_item_outside_menu(); ?>             
-                    </div>
+                </div>
+                    <?php if(! wp_is_mobile()){
+                        kemet_toggle_buttons_markup();
+                        echo kemet_header_custom_item_outside_menu();
+                    } ?> 
              <?php kemet_main_header_bar_bottom(); ?>
             </div> 
         </div> 
