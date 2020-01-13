5c373cbe3ff0ae1f6a73e6d716f20e5a041d1dd7
AbdelRahman <abdelrahman@Remon-Mac-mini.local>
2020-01-13 11:29:06 +0200

    header9-outside-menu-p1

--- addons/extra-headers/templates/header-main-layout-9.php
+++ addons/extra-headers/templates/header-main-layout-9.php
@@ -12,26 +12,30 @@ $icon_label = trim( apply_filters( 'icon_header_label', kemet_get_option( 'heade
     
         <div class="kmt-container">
             <div id="header-layout-9" class="header inline-icon-menu-header">
-                <div class="inline-logo-menu"> 
-                    <?php kemet_site_branding_markup(); ?>
-                    <?php kemet_toggle_buttons_markup(); ?>
-                        <div class="menu-icon-social">
-                            <div class="menu-icon">
-                                <a id="nav-icon" class="icon-bars-btn">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </a>
-                            <?php if(!empty($icon_label)){ ?>
-                                <span class="header-icon-label"><?php echo esc_html( $icon_label ); ?></span>
-                            <?php } ?>    
+                <div class="header9-logo-menu">
+                    <div class="inline-logo-menu"> 
+                        <?php kemet_site_branding_markup(); ?>
+                            <div class="menu-icon-social">
+                                <div class="menu-icon">
+                                    <a id="nav-icon" class="icon-bars-btn">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </a>
+                                <?php if(!empty($icon_label)){ ?>
+                                    <span class="header-icon-label"><?php echo esc_html( $icon_label ); ?></span>
+                                <?php } ?>    
+                                </div>
                             </div>
                         </div>
+                        <div class="kmt-flex main-header-container">
+                            <?php kemet_primary_navigation_markup(); ?>
+                        </div>
                     </div>
-                    <div class="kmt-flex main-header-container">
-                        <?php kemet_primary_navigation_markup(); ?>
+                    <div>
+                    <?php kemet_toggle_buttons_markup(); ?>   
+                    <?php echo kemet_header_custom_item_outside_menu(); ?>             
                     </div>
-                    <?php echo kemet_header_custom_item_outside_menu(); ?> 
              <?php kemet_main_header_bar_bottom(); ?>
             </div> 
         </div> 
