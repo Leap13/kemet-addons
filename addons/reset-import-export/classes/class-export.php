330bc130a354465092b77db2f46eaba4778973a4
Nermin Phelopis <nermin@Nermins-iMac.local>
2020-02-06 14:52:49 +0200

    reset-export-done

--- addons/reset-import-export/classes/class-export.php
+++ addons/reset-import-export/classes/class-export.php
@@ -26,18 +26,11 @@ class Export {
 	 */
 	public function export() {
 		$theme    = get_stylesheet();
-
         // Get options from the Customizer API.
         $theme_options['customizer-settings'] = Kemet_Theme_Options::get_options();
-            
-		// Add Kemet Addons to import.
-		/* if ( class_exists( 'Kemet_Ext_Extension' ) ) {
-			$theme_options['kemet-addons'] = Kemet_Ext_Extension::get_enabled_addons();
-		} */
 
 		$theme_options = apply_filters( 'customizer_export_option_keys', $theme_options );
 
-		// Plugin developers can specify additional option keys to export.
 		$option_keys = apply_filters( 'customizer_export_option_keys', array() );
 
 		foreach ( $option_keys as $option_key ) {
@@ -51,40 +44,12 @@ class Export {
 		nocache_headers();
 		// Set the download headers.
 		header( 'Content-disposition: attachment; filename=customizer-export-of-' . $theme . '.json' );
-		header( 'Content-Type: application/octet-stream; charset=' . $charset );
+		header( 'Content-Type: application/octet-stream; charset=utf-8' );
 
 		// Output the export data.
 		echo wp_json_encode( $theme_options );
 
 		// Start the download.
 		exit;
-
-
-         	// if ( ! isset( $_POST['customizer-export'] ) || ! wp_verify_nonce( $_POST['customizer-export'], 'customizer-export' ) ) {
-			// 	return;
-			// }
-			// if ( empty( $_POST['kemet_ie_action'] ) || 'export_settings' !== $_POST['kemet_ie_action'] ) {
-			// 	return;
-			// }
-			// if ( ! current_user_can( 'manage_options' ) ) {
-			// 	return;
-			// }
-
-			// // Get options from the Customizer API.
-            // $theme_options['customizer-settings'] = Kemet_Theme_Options::get_options();
-            
-			// // Add Kemet Addons to import.
-			// /* if ( class_exists( 'Kemet_Ext_Extension' ) ) {
-			// 	$theme_options['kemet-addons'] = Kemet_Ext_Extension::get_enabled_addons();
-			// } */
-
-			// $theme_options = apply_filters( 'customizer_export_option_keys', $theme_options );
-			// nocache_headers();
-			// header( 'Content-Type: application/json; charset=utf-8' );
-			// header( 'Content-Disposition: attachment; filename=kemet-settings-export-' . date( 'm-d-Y' ) . '.json' );
-			// header( 'Expires: 0' );
-			// echo wp_json_encode( $theme_options );
-			// // Start the download.
-			// die(); 
             }
         }
