60fe88ae8c23352debdefb223d14d12fc3648666
AbdelRahman <abdelrahman@Remon-Mac-mini.local>
2020-03-04 19:44:42 +0200

    woocommerce-p1

--- addons/woocommerce/classes/class-woocommerce-partials.php
+++ addons/woocommerce/classes/class-woocommerce-partials.php
@@ -32,6 +32,78 @@ if (! class_exists('Kemet_Woocommerce_Partials')) {
 		 */
 		public function __construct() {
             add_action( 'kemet_get_css_files', array( $this, 'add_styles' ) );
+            add_action( 'woocommerce_after_shop_loop_item', array( $this, 'kemet_shop_template_loader' ) , 1);
+            add_action( 'woocommerce_before_shop_loop_item', array( $this, 'product_list_details' ) , 8);
+            add_action( 'woocommerce_after_shop_loop_item_title', array( $this, 'after_shop_loop_item_title' ) , 10);
+        }
+
+        
+        function product_list_details() {
+            echo '<div class="product-list-details">';
+        }
+
+       
+        function after_shop_loop_item_title() {
+
+            kemet_woo_woocommerce_template_loop_product_title();
+            echo '</div>';
+        }
+
+        public function kemet_shop_template_loader() {
+            remove_action( 'woocommerce_after_shop_loop_item', 'kemet_woo_woocommerce_shop_product_content' );
+            $shop_structure = apply_filters( 'kemet_woo_shop_product_structure', kemet_get_option( 'shop-product-structure' ) );
+
+            if ( is_array( $shop_structure ) && ! empty( $shop_structure ) ) {
+
+                do_action( 'kemet_woo_shop_before_summary_wrap' );
+                echo '<div class="kemet-shop-summary-wrap">';
+                do_action( 'kemet_woo_shop_summary_wrap_top' );
+                foreach ( $shop_structure as $value ) {
+
+                    switch ( $value ) {
+                        case 'price':
+                            /**
+                             * Add Product Price on shop page for all products.
+                             */
+                            do_action( 'kemet_woo_shop_price_before' );
+                            woocommerce_template_loop_price();
+                            do_action( 'kemet_woo_shop_price_after' );
+                            break;
+                        case 'ratings':
+                            /**
+                             * Add rating on shop page for all products.
+                             */
+                            do_action( 'kemet_woo_shop_rating_before' );
+                            woocommerce_template_loop_rating();
+                            do_action( 'kemet_woo_shop_rating_after' );
+                            break;
+                        case 'short_desc':
+                            do_action( 'kemet_woo_shop_short_description_before' );
+                            kemet_woo_shop_product_short_description();
+                            do_action( 'kemet_woo_shop_short_description_after' );
+                            break;
+                        case 'add_cart':
+                            do_action( 'kemet_woo_shop_add_to_cart_before' );
+                            woocommerce_template_loop_add_to_cart();
+                            do_action( 'kemet_woo_shop_add_to_cart_after' );
+                            break;
+                        case 'category':
+                            /**
+                             * Add and/or Remove Categories from shop archive page.
+                             */
+                            do_action( 'kemet_woo_shop_category_before' );
+                            kemet_woo_shop_parent_category();
+                            do_action( 'kemet_woo_shop_category_after' );
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                do_action( 'kemet_woo_shop_summary_wrap_bottom' );
+                echo '</div>';
+                do_action( 'kemet_woo_shop_after_summary_wrap' );
+            }
         }
 
         function add_styles() {
--- addons/woocommerce/customizer/customizer-options.php
+++ addons/woocommerce/customizer/customizer-options.php
@@ -1,3 +1,30 @@
 <?php
 $defaults = Kemet_Theme_Options::defaults();
-	
+
+/**
+* Option: Single Post Meta
+*/
+$wp_customize->add_setting(
+    KEMET_THEME_SETTINGS . '[shop-product-structure]', array(
+        'default'           => $defaults['shop-product-structure'],
+        'type'              => 'option',
+        'sanitize_callback' => array( 'Kemet_Customizer_Sanitizes', 'sanitize_multi_choices' ),
+    )
+);
+$wp_customize->add_control(
+    new Kemet_Control_Sortable(
+        $wp_customize, KEMET_THEME_SETTINGS . '[shop-product-structure]', array(
+            'type'     => 'kmt-sortable',
+            'section'  => 'section-woo-shop',
+            'priority' => 30,
+            'label'    => __( 'Shop Product Structure', 'kemet' ),
+            'choices'  => array(
+                'price'      => __( 'Price', 'kemet' ),
+                'ratings'    => __( 'Ratings', 'kemet' ),
+                'short_desc' => __( 'Short Description', 'kemet' ),
+                'add_cart'   => __( 'Add To Cart', 'kemet' ),
+                'category'   => __( 'Category', 'kemet' ),
+            ),
+        )
+    )
+);
