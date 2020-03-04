18778b8e9188ac12a71c00949ca7523f65710ca7
AbdelRahman <abdelrahman@Remon-Mac-mini.local>
2020-03-04 14:05:45 +0200

    slider-image

--- addons/single-post/assets/js/unminified/single-post.js
+++ addons/single-post/assets/js/unminified/single-post.js
@@ -1,28 +1,37 @@
 (function ($) {
 
-    var relatedPostsContainer = $('.kmt-related-posts'),
-        desktopItems = relatedPostsContainer.data('desktop'),
-        tabletItems = relatedPostsContainer.data('tablet'),
-        mobileItems = relatedPostsContainer.data('mobile');
-    relatedPostsContainer.owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        autoHeight: true,
-        responsive:{
-            0:{
-                items: mobileItems,
-                nav:true
-            },
-            700:{
-                items: tabletItems,
-                nav:false
-            },
-            1000:{
-                items: desktopItems,
-                nav:true,
-                loop:false
+    var relatedPostsContainer = $('.kmt-related-posts');
+    
+    if(relatedPostsContainer.length > 0 ){
+        var desktopItems = relatedPostsContainer.data('desktop'),
+            tabletItems = relatedPostsContainer.data('tablet'),
+            mobileItems = relatedPostsContainer.data('mobile');
+        relatedPostsContainer.owlCarousel({
+            loop:true,
+            margin:10,
+            responsiveClass:true,
+            autoHeight: true,
+            nav: true,
+            responsive:{
+                0:{
+                    items: mobileItems,
+                },
+                700:{
+                    items: tabletItems,
+                },
+                1000:{
+                    items: desktopItems,
+                }
             }
+        });
+        var squire = function(){
+            var post = $('.kmt-related-posts .related-post > a'),
+            postWidth = post.width();
+            post.height( postWidth );
         }
-    })
+        squire();
+        relatedPostsContainer.on('resized.owl.carousel', function(event) {
+            squire();
+        })
+    }
 })(jQuery);
