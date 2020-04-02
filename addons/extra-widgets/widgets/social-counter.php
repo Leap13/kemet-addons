c20071aa3fc5071f48ea382782f3792d367a0e6d
aaaharidy <a.abdelhamed@leap13.com>
2020-04-02 10:02:18 +0200

    face-book-count

--- addons/extra-widgets/widgets/social-counter.php
+++ addons/extra-widgets/widgets/social-counter.php
@@ -60,24 +60,35 @@ if( ! function_exists( 'kemet_widget_social_counter' ) ) {
     }
 
     $youtube = isset($instance['youtube-channel']) ? $instance['youtube-channel'] : '';
-    $faceboo_access_token = isset($instance['access-token']) ? $instance['access-token'] : '';
-    $faceboo_page_id = isset($instance['fb-page-id']) ? $instance['fb-page-id'] : '';
-    $faceboo_name = isset($instance['fb-page-name']) ? $instance['fb-page-name'] : '';
-    var_dump($faceboo_name);
-    if(!empty($faceboo_access_token) && !empty($faceboo_page_id) && !empty($faceboo_name)){
-      $fb_count = facebook_count($faceboo_page_id , $faceboo_access_token);
-      var_dump($fb_count);
-    }
+    $fb_fields = isset($instance['facebook-fields']) ? $instance['facebook-fields'] : '';
+    $faceboo_access_token = isset($fb_fields['access-token']) ? $fb_fields['access-token'] : '';
+    $faceboo_page_id = isset($fb_fields['fb-page-id']) ? $fb_fields['fb-page-id'] : '';
+    $faceboo_name = isset($fb_fields['fb-page-name']) ? $fb_fields['fb-page-name'] : '';
+    
+    
     $html = '<div class="kmt-social-counter">';
     $html .= '<ul>';
     if(!empty($youtube)):
       $api_key = "AIzaSyAGzk0XPlUBwfgLpuOAljqG5ruDOCrntas";
       $url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id='.$youtube.'&fields=items/statistics/subscriberCount&key='.$api_key;
-      $obj = json_decode(get_youtube_subs($url), true);
+      $obj = json_decode(get_api_result($url), true);
       $html .= '<li>';
-      $html .= '<a href="https://www.youtube.com/channel/'.$youtube.'">';
+      $html .= '<a href="https://www.youtube.com/channel/'.$youtube.'" target="_blank">';
       $html .= '<span class="count-icon dashicons dashicons-video-alt3"></span>';
       $html .= '<div class="subscriber"><span>'.$obj['items'][0]['statistics']['subscriberCount'].'</span><small>Subscribers</small></div>';
+      $html .= '</a>';
+      $html .= '</li>';
+    endif;
+    if(!empty($faceboo_access_token) && !empty($faceboo_page_id) && !empty($faceboo_name)):
+
+      $api_url = 'https://graph.facebook.com/' . $faceboo_page_id . '/?fields=fan_count&access_token=' . $faceboo_access_token;
+      $fb_obj = get_api_result($api_url);
+      $fb_fan_obj = json_decode($fb_obj, true);
+
+      $html .= '<li>';
+      $html .= '<a href="https://www.facebook.com/'.$faceboo_page_id.'" target="_blank">';
+      $html .= '<span class="count-icon dashicons dashicons-facebook-alt"></span>';
+      $html .= '<div class="subscriber"><span>'.$fb_fan_obj['fan_count'].'</span><small>Likes</small></div>';
       $html .= '</a>';
       $html .= '</li>';
     endif;
@@ -88,7 +99,7 @@ if( ! function_exists( 'kemet_widget_social_counter' ) ) {
     echo $args['after_widget']; 
   } 
 }
-function get_youtube_subs($url,$useragent='cURL',$headers=false, $follow_redirects=false,$debug=false) {
+function get_api_result($url,$useragent='cURL',$headers=false, $follow_redirects=false,$debug=false) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,$url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
@@ -112,15 +123,5 @@ function get_youtube_subs($url,$useragent='cURL',$headers=false, $follow_redirec
 
     curl_close($ch);
     return $result;
-}
-/**
- * Gets JSON Data from Facebook
- */
-function facebook_count( $page_id, $page_access_token ) { 
-    $api_url = 'https://graph.facebook.com/' . $page_id . "/?fields=fan_count&access_token=" . $page_access_token;
-    
-    $api_response = rplg_urlopen( $api_url );
-  
-    return $api_response;
 }
 register_widget( Kemet_Create_Widget::instance( "kemet_widget_social_counter" , $social_counter_widgets) );
