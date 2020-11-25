0c543808da54d724abf9eca7e8656ed34a6098af
AbdelRahman <abdelrahman@Remon-Mac-mini.local>
2020-11-25 21:13:58 +0200

    options default

--- inc/kemet-panel/tabs/class-kemet-panel-options.php
+++ inc/kemet-panel/tabs/class-kemet-panel-options.php
@@ -68,50 +68,50 @@ if (! class_exists('Kemet_Panel_options_Tab')) {
                 'type' => 'switcher',
                 'title' => __('Advanced Headers', 'kemet-addons'),
                 'subtitle' => __('Enable/Disable more header layouts and no header option in both customizer and page/post options.', 'kemet-addons'),
-                'default' => true,
+                'default' => false,
                 ),array(
                 'id' => 'sticky-header',
                 'type' => 'switcher',
                 'title' => __('Sticky Header', 'kemet-addons'),
                 'subtitle' => __('Enable/Disable Kemet sticky header options.', 'kemet-addons'),
-                'default' => true,
+                'default' => false,
                 ),array(
                 'id' => 'top-bar-section',
                 'type' => 'switcher',
                 'title' => __('Top Bar Section', 'kemet-addons'),
                 'subtitle' => __('Enable/Disable the top bar area that includes right and left sections with unlimited content possibilities.', 'kemet-addons'),
-                'default' => true,
+                'default' => false,
                 ),array(
                 'id' => 'page-title',
                 'type' => 'switcher',
                 'title' => __('Page Title', 'kemet-addons'),
                 'subtitle' => __('Enable/Disable the page title area that includes page/post title and breadcrumbs.', 'kemet-addons'),
-                'default' => true,
+                'default' => false,
                 ),array(
                 'id' => 'go-top',
                 'type' => 'switcher',
                 'title' => __('Go to Top Button', 'kemet-addons'),
                 'subtitle' => __('Enable/Disable the Go to Top button including its customization options.', 'kemet-addons'),
-                'default' => true,
+                'default' => false,
                 ),array(
                 'id' => 'extra-widgets',
                 'type' => 'switcher',
                 'title' => __('Extra Widgets', 'kemet-addons'),
                 'subtitle' => __('Enable/Disable Kemet extra widgets that have been built to enrich your WordPress website.', 'kemet-addons'),
-                'default' => true,
+                'default' => false,
                 ),array(
                 'id' => 'blog-layouts',
                 'type' => 'switcher',
                 'title' => __('Blog Layouts', 'kemet-addons'),
                 'subtitle' => __('Enable/Disable Extra Blog Layouts', 'kemet-addons'),
-                'default' => true,
+                'default' => false,
                 ),
                 array(
                 'id' => 'single-post',
                 'type' => 'switcher',
                 'title' => __('Single Post Options', 'kemet-addons'),
                 'subtitle' => __('Enable/Disable the extra options that will allow you to customize your single post content.', 'kemet-addons'),
-                'default' => true,
+                'default' => false,
                 ),array(
                 'id' => 'custom-layout',
                 'type' => 'switcher',
@@ -123,7 +123,7 @@ if (! class_exists('Kemet_Panel_options_Tab')) {
                 'type' => 'switcher',
                 'title' => __('Mega Menu', 'kemet-addons'),
                 'subtitle' => __('Enable/Disable Mega Menu', 'kemet-addons'),
-                'default' => true,
+                'default' => false,
                 ),
             );
 
@@ -132,7 +132,7 @@ if (! class_exists('Kemet_Panel_options_Tab')) {
                 'type' => 'switcher',
                 'title' => __('Woocommerce', 'kemet-addons'),
                 'subtitle' => __('Enable/Disable the extra options that allows you to control & customize WooCommerce product page and product listing.', 'kemet-addons'),
-                'default' => true,
+                'default' => false,
             );
             if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
                 $woo_option += ['class' => 'hidden-field-input'];
