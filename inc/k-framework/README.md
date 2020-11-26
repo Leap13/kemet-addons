7d7c682d9522e1807853c0d09f921c5c92ff63e2
aaaharidy <a.abdelhamed@leap13.com>
2020-11-26 17:56:28 +0200

    wiz to kemet

--- inc/k-framework/README.md
+++ inc/k-framework/README.md
@@ -1,7 +1,9 @@
 # K Framework
-K Framework is a lightweight WordPress options framework developed to be used for Wiz WordPress Theme and addon plugins.
+
+K Framework is a lightweight WordPress options framework developed to be used for Kemet WordPress Theme and addon plugins.
 
 ## Available Fileds
+
 - Checkbox
 - Switcher
 - Select
--- inc/k-framework/k-framework.php
+++ inc/k-framework/k-framework.php
@@ -4,18 +4,18 @@
 
 /**
  *
- * @package   Wiz Framework - WordPress Options Framework
+ * @package   Kemet Framework - WordPress Options Framework
  * @author    Leap13
- * @link      https://themes.leap13.com/wiz
+ * @link      https://kemet.io
  * @copyright 2019 Leap13
  *
  *
- * Plugin Name: Wiz Framework
- * Plugin URI: https://themes.leap13.com/wiz
+ * Plugin Name: Kemet Framework
+ * Plugin URI: https://kemet.io
  * Author: Leap13
  * Author URI: https://leap13.com
  * Version: 0.1.0
- * Description: K Framework is a lightweight WordPress options framework developed to be used for Wiz WordPress Theme and addon plugins.
+ * Description: K Framework is a lightweight WordPress options framework developed to be used for Kemet WordPress Theme and addon plugins.
  * Text Domain: kfw
  * Domain Path: /languages
  *
