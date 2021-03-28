7447476dfaa7fb8a15253abd15b181d3f1f644d6
Abdelrhman Haridy <a.abdelhamed@leap13.com>
2021-03-28 18:13:08 +0200

    extra widgets addon

--- addons/extra-widgets/widgets/posts-list.php
+++ addons/extra-widgets/widgets/posts-list.php
@@ -1,61 +1,75 @@
 <?php
+/**
+ * Posts List Widget.
+ *
+ * @package Kemet Addons
+ */
+
 $posts_list_widget = array(
-  'title'       => __('Kemet Posts List', 'kemet-addons' ),
-  'classname'   => 'kfw-widget-posts-list',
-  'id'          => 'kemet-widget-posts-list',
-  'description' => __('Posts List', 'kemet-addons' ),
-  'fields'      => array(
-    array(
-      'id'      => 'title',
-      'type'    => 'text',
-      'title'   => __('Title:', 'kemet-addons' ),
-      'default'   => __('Posts List', 'kemet-addons' ),
-    ),
-    array(
-      'id'    => 'posts-number',
-      'type'  => 'number',
-      'title' => __('Number of posts to show', 'kemet-addons' ),
-      'default'     => 5
-    ),
-    array(
-      'id'          => 'posts-order',
-      'type'        => 'select',
-      'title'       => __('Posts Order', 'kemet-addons' ),
-      'options'     => array(
-            'most-recent' => __('Most Recent', 'kemet-addons' ),
-            'popular' => __('Popular', 'kemet-addons' ),
-            'random' => __('Random', 'kemet-addons' ),
-      ),
-      'default'     => 'most-recent'
-    ),
-    array(
-        'id'      => 'display-thumbinals',
-        'type'    => 'checkbox',
-        'title'   => __('Display Thumbinals', 'kemet-addons' ),
-        'default' => true
-      ),
-  )
+	'title'       => __( 'Kemet Posts List', 'kemet-addons' ),
+	'classname'   => 'kfw-widget-posts-list',
+	'id'          => 'kemet-widget-posts-list',
+	'description' => __( 'Posts List', 'kemet-addons' ),
+	'fields'      => array(
+		array(
+			'id'      => 'title',
+			'type'    => 'text',
+			'title'   => __( 'Title:', 'kemet-addons' ),
+			'default' => __( 'Posts List', 'kemet-addons' ),
+		),
+		array(
+			'id'      => 'posts-number',
+			'type'    => 'number',
+			'title'   => __( 'Number of posts to show', 'kemet-addons' ),
+			'default' => 5,
+		),
+		array(
+			'id'      => 'posts-order',
+			'type'    => 'select',
+			'title'   => __( 'Posts Order', 'kemet-addons' ),
+			'options' => array(
+				'most-recent' => __( 'Most Recent', 'kemet-addons' ),
+				'popular'     => __( 'Popular', 'kemet-addons' ),
+				'random'      => __( 'Random', 'kemet-addons' ),
+			),
+			'default' => 'most-recent',
+		),
+		array(
+			'id'      => 'display-thumbinals',
+			'type'    => 'checkbox',
+			'title'   => __( 'Display Thumbinals', 'kemet-addons' ),
+			'default' => true,
+		),
+	),
 );
 
-if( ! function_exists( 'kemet_widget_posts_list' ) ) {
-  function kemet_widget_posts_list( $args, $instance ,$id) {
-    echo $args['before_widget'];
-    if ( ! empty( $instance['title'] ) ) {
-      echo $args['before_title'] . apply_filters( 'widget_title', esc_attr($instance['title'], 'kemet-addons' ) ) . $args['after_title'];
-    }
-    global $post;
-    $orig_post = $post;
+if ( ! function_exists( 'kemet_widget_posts_list' ) ) {
+	/**
+	 * Create widget
+	 *
+	 * @param object $args args.
+	 * @param object $instance instance.
+	 * @param int    $id widget id.
+	 * @return void
+	 */
+	function kemet_widget_posts_list( $args, $instance, $id ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', esc_html( $instance['title'] ) ) . $args['after_title'];
+		}
+		global $post;
+		$orig_post = $post;
 
-    $order = isset($instance['posts-order']) ? $instance['posts-order'] : 'random';
-    $posts_number = isset($instance['posts-number']) ? $instance['posts-number'] : 5;
-    $display_thumbnail = isset($instance['display-thumbinals']) ? $instance['display-thumbinals'] : true;
-    $query = array(
+		$order             = isset( $instance['posts-order'] ) ? $instance['posts-order'] : 'random';
+		$posts_number      = isset( $instance['posts-number'] ) ? $instance['posts-number'] : 5;
+		$display_thumbnail = isset( $instance['display-thumbinals'] ) ? $instance['display-thumbinals'] : true;
+		$query             = array(
 			'posts_per_page'      => $posts_number,
 			'no_found_rows'       => true,
 			'post_status'         => 'publish',
 			'ignore_sticky_posts' => true,
 		);
-    switch ( $order ) {
+		switch ( $order ) {
 			case 'random':
 				$query['orderby'] = esc_html( 'rand' );
 				break;
@@ -64,10 +78,10 @@ if( ! function_exists( 'kemet_widget_posts_list' ) ) {
 				break;
 		}
 		$all_posts = new WP_Query( $query );
-		if ( $all_posts->have_posts() ) {       
-    ?>
-    <ul class="kmt-wdg-posts-list">
-    <?php
+		if ( $all_posts->have_posts() ) {
+			?>
+	<ul class="kmt-wdg-posts-list">
+			<?php
 			foreach ( $all_posts->posts as $q_post ) {
 				$post_title = get_the_title( $q_post->ID );
 				$title      = ( ! empty( $post_title ) ) ? $post_title : __( '(no title)' );
@@ -85,12 +99,12 @@ if( ! function_exists( 'kemet_widget_posts_list' ) ) {
 				</li>
 				<?php
 			}
-      ?>
-    </ul>
-    <?php
-    }
-    echo $args['after_widget']; 
-  } 
+			?>
+	</ul>
+			<?php
+		}
+		echo $args['after_widget'];
+	}
 }
 
-register_widget( Kemet_Create_Widget::instance( "kemet_widget_posts_list" , $posts_list_widget) );
+register_widget( Kemet_Addon_Create_Widget::instance( 'kemet_widget_posts_list', $posts_list_widget ) );
