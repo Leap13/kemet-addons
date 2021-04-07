9d11cc22632126687bd73cccaae5460d563c8e4e
Abdelrhman Haridy <a.abdelhamed@leap13.com>
2021-04-07 14:47:10 +0200

    merge topbar & disable separator meta

--- addons/top-bar-section/classes/class-kemet-addon-top-bar-metabox.php
+++ addons/top-bar-section/classes/class-kemet-addon-top-bar-metabox.php
@@ -70,6 +70,20 @@ if ( ! class_exists( 'Kemet_Addon_Top_Bar_Metabox' ) ) {
 					'priority_num' => 2,
 					'fields'       => array(
 						array(
+							'id'      => 'kemet-merge-top-bar-with-header',
+							'type'    => 'checkbox',
+							'title'   => __( 'Merge/Combine Top Bar With Main Header', 'kemet-addons' ),
+							'label'   => __( 'Merge/Combine Top Bar With Main Header in The Current Page/Post.', 'kemet-addons' ),
+							'default' => false,
+						),
+						array(
+							'id'      => 'kemet-disable-top-bar-separators',
+							'type'    => 'checkbox',
+							'title'   => __( 'Disable top bar separators', 'kemet-addons' ),
+							'label'   => __( 'Disable top bar separators in The Current Page/Post.', 'kemet-addons' ),
+							'default' => false,
+						),
+						array(
 							'id'      => 'kemet-top-bar-display',
 							'type'    => 'checkbox',
 							'title'   => __( 'Disable Top Bar', 'kemet-addons' ),
