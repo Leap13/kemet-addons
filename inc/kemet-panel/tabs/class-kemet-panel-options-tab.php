be5c65af7a955a8e05464d27b035cbfc471c08f1
aaaharidy <a.abdelhamed@leap13.com>
2021-04-20 13:26:22 +0200

    wp_kses error

--- inc/kemet-panel/tabs/class-kemet-panel-options-tab.php
+++ inc/kemet-panel/tabs/class-kemet-panel-options-tab.php
@@ -138,13 +138,6 @@ if ( ! class_exists( 'Kemet_Panel_Options_Tab' ) ) {
 					'subtitle' => __( 'Enable/Disable Mega Menu', 'kemet-addons' ),
 					'default'  => false,
 				),
-				array(
-					'id'       => 'custom-fonts',
-					'type'     => 'switcher',
-					'title'    => __( 'Custom Fonts', 'kemet-addons' ),
-					'subtitle' => __( 'Enable/Disable Custom fonts.', 'kemet-addons' ),
-					'default'  => false,
-				),
 			);
 
 			$woo_option = array(
