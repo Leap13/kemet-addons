95b084d81a722e98cfdb9f4a6276657408ffcb4a
Abdelrhman Haridy <a.abdelhamed@leap13.com>
2021-05-23 14:55:32 +0200

    menu item in theme

--- kemet-addons.php
+++ kemet-addons.php
@@ -3,7 +3,7 @@
  * Plugin Name: Kemet Addons
  * Plugin URI: https://kemet.io
  * Description: This Plugin for Kemet Theme
- * Version: 1.1.17
+ * Version: 1.1.20
  * Author: Leap13
  * Author URI: https://leap13.com
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
